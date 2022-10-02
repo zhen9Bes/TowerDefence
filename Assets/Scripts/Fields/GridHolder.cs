e1fad93ed8cd9b5fe03e43175613acacf907d889
3vgen <56633537+zhen9Bes@users.noreply.github.com>
2022-10-02 13:43:33 +0700

    Add GridHolder class

---
+++ Assets/Scripts/Fields/GridHolder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Fields
+{
+	internal class GridHolder : MonoBehaviour
+	{
+		[SerializeField]
+		private int _gridWidth;
+		[SerializeField]
+		private int _gridHeight;
+		private Grid _grid;
+		private Camera _camera;
+
+		private void Awake()
+		{
+			_grid = new Grid(_gridWidth, _gridHeight);
+			_camera = Camera.main;
+		}
+
+		private void Update()
+		{
+			if(_grid == null || _camera == null)
+			{
+				return;
+			}
+
+			var mousePosition = Input.mousePosition;
+			var ray = _camera.ScreenPointToRay(mousePosition);
+
+			if(Physics.Raycast(ray, out RaycastHit hit))
+			{
+				if(hit.transform != transform)
+				{
+					return;
+				}
+
+				Debug.Log("hit!");
+			}
+		}
+	}
+}
