1dbf090feb8701756495428c5584aa7a47419816
3vgen <56633537+zhen9Bes@users.noreply.github.com>
2022-10-02 14:43:37 +0700

    Add find raycast coordinates.

--- Assets/Scripts/Fields/GridHolder.cs
+++ Assets/Scripts/Fields/GridHolder.cs
@@ -11,20 +11,32 @@ namespace TowerDefence.Scripts.Fields
 	{
 		[SerializeField]
 		private int _gridWidth;
+
 		[SerializeField]
 		private int _gridHeight;
+
+		[SerializeField]
+		private float _nodeSize;
+
 		private Grid _grid;
 		private Camera _camera;
+		private Vector3 _offset;
 
 		private void Awake()
 		{
 			_grid = new Grid(_gridWidth, _gridHeight);
 			_camera = Camera.main;
+
+			float width = _gridWidth * _nodeSize;
+			float height = _gridHeight * _nodeSize;
+			// Default polane size is 10 by 10
+			transform.localScale = new Vector3(width * 0.1f, 1f, height * 0.1f);
+			_offset = transform.position - (new Vector3(width, 0, height) * 0.5f);
 		}
 
 		private void Update()
 		{
-			if(_grid == null || _camera == null)
+			if (_grid == null || _camera == null)
 			{
 				return;
 			}
@@ -32,15 +44,27 @@ namespace TowerDefence.Scripts.Fields
 			var mousePosition = Input.mousePosition;
 			var ray = _camera.ScreenPointToRay(mousePosition);
 
-			if(Physics.Raycast(ray, out RaycastHit hit))
+			if (Physics.Raycast(ray, out RaycastHit hit))
 			{
-				if(hit.transform != transform)
+				if (hit.transform != transform)
 				{
 					return;
 				}
 
-				Debug.Log("hit!");
+				Vector3 hitPosition = hit.point;
+				Vector3 difference = hitPosition - _offset;
+
+				var x = (int)(difference.x / _nodeSize);
+				var y = (int)(difference.z / _nodeSize);
+
+				Debug.Log(x + " " + y);
 			}
+		}
+
+		private void OnDrawGizmos()
+		{
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawSphere(_offset, 0.1f);
 		}
 	}
 }
