e6c4c80057dc05c86c99d6fa2e8c5dc9413385cc
3vgen <56633537+zhen9Bes@users.noreply.github.com>
2022-10-02 13:52:04 +0700

    Change namespace to TowerDefence

--- Assets/Scripts/Fields/GridHolder.cs
+++ Assets/Scripts/Fields/GridHolder.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Assets.Scripts.Fields
+namespace TowerDefence.Scripts.Fields
 {
 	internal class GridHolder : MonoBehaviour
 	{
